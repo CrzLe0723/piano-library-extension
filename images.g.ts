68fe4fbd4a93882256c24df91a8b655a13ffba85
Cruz Le <Cruzle0723@gmail.com>
2026-09-14 19:27:52 -0700

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,34 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "TestSong":return hex`0078000408020100001c00010a006400f401640000040000000000000000000000000005000006300004000800010b0400080001050400080001120c00100001050c001000010d18001c00010c18001c000103240028000104`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,1 @@
+music.play(pianoRoll.createSong(assets.song`TestSong`), music.PlaybackMode.UntilDone)
