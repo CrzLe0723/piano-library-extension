817cad7006aeb6651694ca9df135f4476a7e0355
Cruz Le <Cruzle0723@gmail.com>
2026-09-14 20:33:56 -0700

    Update pxt.json, music.g.jres, gallery.ts, images.g.jres, images.g.ts, test.ts

---
+++ gallery.ts
@@ -0,0 +1,7 @@
+// Auto-generated code. Do not edit.
+namespace Piano_Library_Extension {
+    //% fixedInstance jres whenUsed
+    export const TestSong = hex`0078000408020100001c00010a006400f401640000040000000000000000000000000005000006300004000800010b0400080001050400080001120c00100001050c001000010d18001c00010c18001c000103240028000104`;
+
+}
+// Auto-generated code. Do not edit.
